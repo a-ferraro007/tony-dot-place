4d7acfdc0ce2482cfb562fed84dc860964a890af
Anthony Ferraro <tony@anthonys-mbp.mynetworksettings.com>
2024-03-22 01:03:53 -0400

    feat: new look

--- app/Components/main.tsx
+++ app/Components/main.tsx
@@ -1,11 +1,24 @@
-import SubComponent from "./SubComponent/subComponent";
-
 export default function Main() {
   return (
-    <div className="flex flex-col items-center mt-16 gap-2">
-      <h1 className="text-6xl text-[#f0f8ff]"> 🚧 Site In Progress 🚧</h1>
-        <SubComponent />  
-    </div>
-  ) 
+    <>
+      <div>
+        <div className="h-full flex flex-col ml-36 mt-48">
+          <h1 className="text-6xl font-extralight mb-2">Anthony Ferraro</h1>
+          <h2 className="ml-2 text-2xl font-light"> Software Enginer -
+            <span className="text-2xl font-semibold text-[#F45A27]"> LegalZoom </span></h2>
+        </div>
+      </div>
+      <div className="fixed bottom-0 w-full flex justify-center p-1">
+        <p className="font-semibold">
+          made with{" "}
+          <a
+            className="border-b-[1.5px] border-black pb-[1px]"
+            href="https://github.com/a-ferraro007/OliveJs/tree/main"
+          >
+            🫒
+          </a>
+        </p>
+      </div>
+    </>
+  );
 }
-  
